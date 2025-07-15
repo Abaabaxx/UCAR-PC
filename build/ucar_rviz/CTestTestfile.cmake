7b8d1e11bafcde487399adbf23430e57f9cb7daa
abaabaxxx <agt2643194651@outlook.com>
2025-07-15 14:52:03 +0800

    停止追踪devel build等文件

--- build/ucar_rviz/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lby/ucar_ws/src/ucar_rviz
-# Build directory: /home/lby/ucar_ws/build/ucar_rviz
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
